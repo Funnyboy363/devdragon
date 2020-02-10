f92fd153462558523b935e92c962ec8338692994
Grant <grantshoop@gmail.com>
2020-02-10 14:04:04 -0700

    adjustments for livesite

--- php/mail.php
+++ php/mail.php
@@ -29,7 +29,7 @@ $content="From: $name \nEmail: $email \nMessage: $message";
 $recipient = "grantshoop@gmail.com";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $content, $mailheader) or die("Error!");
-print json_encode(array('message'<?php
+print json_encode(array('message' <?php>
 $name = $_POST['name'];
 $email = $_POST['email'];
 $message = $_POST['message'];
